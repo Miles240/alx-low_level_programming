f81d551856542ec3b677b6b499a683f1b78e2a47
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-09-13 00:36:12 +0100

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - takes two intergers and return there sum
+ * @i: The first integer
+ * @n: The second integer
+ * Return: always 0
+ */
+
+int add(int i, int n)
+{
+
+	int m = i + n;
+
+	return (m);
+}
