0c76452349a904cfbda18658550b201595317391
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-09-14 23:18:00 +0100

    test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("Fizz Buzz ");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
