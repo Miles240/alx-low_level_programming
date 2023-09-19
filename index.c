2b473b7b1bd09d480c1b985d90a330872c9f9d73
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-09-19 01:33:28 +0100

    !c_is_fun

---
+++ index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void){
+
+    char *str = "1234567";
+    int len = strlen(str- 1) / 2;
+    int i;
+    for (i = 0; i <= len; i++){
+        printf("%d", len);
+    }
+
+    return (0);
+}
