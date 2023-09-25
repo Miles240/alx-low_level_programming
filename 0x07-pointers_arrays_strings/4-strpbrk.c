61548ae5f237a8f6bad4c6235e6206cbc9be1bbb
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-09-25 16:05:17 +0100

    .

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,9 +6,12 @@
  * @accept: matches one of the bytes, or @NULL if no such byte
  * Return: a pointer to the byte
  */
+
 char *_strpbrk(char *s, char *accept)
 {
+
 	int i;
+	
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
