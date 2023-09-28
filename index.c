c2ffd7a019dc08e95bf62e45c720348782cc1116
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-09-29 00:36:40 +0100

    !c_is_fun

--- index.c
+++ index.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main (void){
-
-    char *str = "1234567";
-    int len = strlen(str- 1) / 2;
-    int i;
-    for (i = 0; i <= len; i++){
-        printf("%d", len);
-    }
-
-    return (0);
+int main(){
+    
 }
+
---
+++ lib.h
@@ -0,0 +1,6 @@
+#ifndef lib_h
+#define lib_h
+
+typedef char *string;
+
+#endif
