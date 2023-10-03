addf45ad083a020fdd2942ad87bf7d58de43e28a
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-03 16:11:09 +0100

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,16 +7,19 @@
 
 char *_strdup(char *str)
 {
-	int i;
+	int i, len;
 	char *newStr = NULL;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	newStr = malloc(sizeof(str) + 1);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (len = 0; str[len] != '\0'; len++)
+
+		newStr = malloc((len + 1) * sizeof(char));
+
+	for (i = 0; i < len; i++)
 	{
 		newStr[i] = str[i];
 	}
