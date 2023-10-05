27f469e47c8472749cb2fc7440729db210c98ba0
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-05 12:10:43 +0100

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,10 +18,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		return (NULL);
 	}else if (s2 == NULL)
 	{
-		return ("");
+		return (NULL);
 	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
