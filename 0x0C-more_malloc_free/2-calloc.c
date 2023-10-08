5d7f8760e43e7097cdaa7bf0f4abf3b48dec2ca0
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-08 17:02:01 +0100

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate memory for an array using malloc
+ * @nmemb: number of elements in the array
+ * @size: amount of bytes of each element
+ *
+ * Return: pointer to the newly allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(nmemb * sizeof(size));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
