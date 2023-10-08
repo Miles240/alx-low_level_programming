bf342a40d83a1b448c01da6edbd392bc55acbd7c
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-08 17:31:38 +0100

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * string_nconcat - allocate a new memory and store the values of
- * s1 and the n bytes of s2
- * @s1: the first string
- * @s2: second string
- * @n: number of byte needed for s2
- * 
- * Return: a pointer to a new allocated string 
+ *string_nconcat - allocate a new memory and store the values of
+ *s1 and the n bytes of s2
+ *@s1: the first string
+ *@s2: second string
+ *@n: number of byte needed for s2
+ *
+ *Return: a pointer to a new allocated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -22,7 +22,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 		}
 	}
-
 	if (s2 != NULL)
 	{
 		b = 0;
@@ -31,15 +30,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			b++;
 		}
 	}
-
 	newlen = a + b;
 	newStr = malloc(sizeof(char) * (newlen + 1));
-
 	if (newStr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (d = 0; d < a; d++)
 	{
 		newStr[d] = s1[d];
