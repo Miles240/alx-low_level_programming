dd11f2873a8115bc74e5f32635a9d3f38bfa2001
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-10 10:38:29 +0100

    doggy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog.h
+#define dog.h
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+}
+
+#endif
