c3da980a9a81c55884a619aa663c723de1f27f38
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-30 21:05:36 +0100

    updated to betty standard

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,8 +21,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
 		return (0);
-
+	}
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
